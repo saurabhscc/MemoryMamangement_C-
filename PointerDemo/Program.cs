8f9ef9b0699a9133f71dd5019c337d444a24b372
saurabhs <saurabhpshirsath@gmail.com>
2021-11-17 14:15:58 +0530

    [saurabh] Add:Pointer in csharp

---
+++ PointerDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PointerDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            unsafe
+            {
+                int var = 20;
+                int* p = &var;//address area to pointer p
+                Console.WriteLine("Data is : {0}", var);
+                Console.WriteLine("Address is:{0}", (int)p);
+                Console.ReadLine();
+            }
+        }
+    }
+}
