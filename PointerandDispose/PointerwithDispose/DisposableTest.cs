486ecee9f5048d0a4a57eee63fbe9514068a0f03
saurabhs <saurabhpshirsath@gmail.com>
2021-11-17 14:18:56 +0530

    [saurabh] Add:Pointer with structures

---
+++ PointerandDispose/PointerwithDispose/DisposableTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointerwithDispose
+{
+    class DisposableTest : IDisposable
+    {
+        private bool disposed = false;
+        public void Print(string message)
+        {
+            Console.WriteLine("Hello"+ message);
+        }
+        ~DisposableTest()
+        {
+            Console.WriteLine("Destructor/finalize of DisposableTest");
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected void Dispose(bool dispose)
+        {
+            if (!disposed)
+            {
+                if (dispose)
+                {
+                    Console.WriteLine("Cleanup Managed Object");
+                }
+                Console.WriteLine("Cleanup unmanaged Object");
+                disposed = true;
+            }
+        }
+    }
+}
+
---
+++ PointerandDispose/PointerwithDispose/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PointerwithDispose
+{
+    struct MyStruct
+    {
+        public int x;
+        public int y;
+        public void SetXY(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+        public void ShowXY()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+    class MyClient
+    {
+        public unsafe static void Main()
+        {
+
+            MyStruct ms = new MyStruct();
+            MyStruct* ms1 = &ms;
+            ms1->SetXY(10, 20);
+            ms1->ShowXY();
+            DisposableTest dt = new DisposableTest();
+            Console.WriteLine("DisposableTest Object created...");
+            dt.Print("");
+            dt.Dispose();
+            dt = null;
+            Console.WriteLine("Assigned Null... Object Destructing...");
+            Console.ReadLine();
+
+        }
+    }
+}
